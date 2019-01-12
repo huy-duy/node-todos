95f71f563cbbc55b89c3d584762e156a39008920
Huy Duy <huyduy.work@gmail.com>
2019-01-12 20:31:47 +0700

    :sparkles: add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -44,6 +44,22 @@ app.get('/todos/:id', (req, res) => {
   })
 });
 
+app.delete('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if(!ObjectID.isValid(id))
+    return res.status(404).send();
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo)
+      return res.status(404).send();
+    res.send(todo);
+  }).catch((e) => {
+    res.status(404).send();
+  })
+  
+});
+
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
